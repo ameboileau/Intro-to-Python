45d0df87bab4e6ef8bd230bf9079f0e7645f768b
Jade Ducharme <jducharme@transcanadacapital.com>
2022-08-18 23:06:54 -0400

    Week 9 PPT and Exercises completed

---
+++ Exercises/Week9/Week9.py
@@ -0,0 +1,131 @@
+# 
+# Introduction to Python - Week 9 exercises
+#
+
+# import list
+import numpy as np
+import pandas as pd
+
+
+
+# Exercise 1:
+# Write a function that takes as input a list of 5 numbers and calculates the following quantities:
+# the mean
+# the median
+# the sum
+# the mode
+# Hint: Use numpy!
+
+
+def exercise_1(l):
+    """
+    Function description goes here
+    """
+
+    #
+    # Your code goes here
+    #
+
+
+
+
+# Exercise 2:
+# Write a function that takes as input two strings and combines them into one
+
+
+def exercise_2(s1, s2):
+    """
+    Function description goes here
+    """
+
+    #
+    # Your code goes here
+    #
+
+
+
+# Exercise 3:
+# Write a function that takes as input three lists and transforms them into a pandas DataFrame
+# where the content of the lists make up the DataFrame columns
+
+
+def exercise_3(l1, l2, l3):
+    """
+    Function description goes here
+    """
+
+    #
+    # Your code goes here
+    #
+
+
+
+# ======================================================================================================
+
+
+
+# Exercise 4:
+# Write a function that takes as input a list of numbers and increases their value by their index
+
+
+def exercise_4(l):
+    """
+    Function description goes here
+    """
+
+    #
+    # Your code goes here
+    #
+
+
+
+# Exercise 5:
+# Write a function that takes as input a list of numbers and calculates a list corresponding
+# to the cumulative sum 
+# e.g. input --> [0, 1, 2, 3, 4, 5]
+# output --> [0, 1, 3, 6, 10, 15]
+
+
+def exercise_5(l):
+    """
+    Function description goes here
+    """
+
+    #
+    # Your code goes here
+    #
+
+
+
+# ======================================================================================================
+
+
+
+# Exercise 6:
+# Write a function that takes as input a variable v and tries to divide it by 2
+# The code should print a meaningful message if it encounters an exception
+
+'''
+Your code goes here
+'''
+
+
+# Exercise 7:
+# Write a function that takes as input a list and tries to calculate the sum
+# The code should print a meaningful message if it encounters an exception
+# Regardless of whether the code encounters an error, it should print out 'Code execution completed.'
+
+'''
+Your code goes here
+'''
+
+
+
+# ------------------------------------------------------------------------------------------------------
+#
+# Run your code below!
+#
+# ------------------------------------------------------------------------------------------------------
+
+
+
